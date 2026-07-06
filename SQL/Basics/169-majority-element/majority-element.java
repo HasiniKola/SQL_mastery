8a3731cd1f6cfc8e1fe12ebad1df38ede9979480
Hasini Kola <153356309+HasiniKola@users.noreply.github.com>
2026-07-06 18:46:54 +0530

    Time: 1929 ms (5.06%) | Memory: 55.7 MB (31.65%) - LeetSync

---
+++ SQL/Basics/169-majority-element/majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+
+        for (int val : nums) {
+            int freq = 0;
+
+            for (int el : nums) {
+                if (el == val) {
+                    freq++;
+                }
+            }
+
+            if (freq > n / 2) {
+                return val;
+            }
+        }
+
+        // If no majority element found (though problem guarantees one)
+        return -1;
+    }
+}
